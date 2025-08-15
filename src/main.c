440e0714e2c763c137cd2f1aee47050ad9e810c8
leo <lleeooooo0209@gmail.com>
2025-08-15 20:03:47 +0800

    changed movement input. no longer click to move but rather click to change direction.

--- src/main.c
+++ src/main.c
@@ -9,54 +9,61 @@
 
 WINDOW *initCursesWindow();
 
-int frameMicroSec = 100000;
+int frameMicroSec = 50000;
+int framePerMove = 5;
 
 int main()
 {
     initscr();
 
     WINDOW *win = initCursesWindow();
-    
 
     Map map;
-    if(!initMap(&map)){
+    if (!initMap(&map))
+    {
         return 0;
     }
     struct node start;
-    if(!initEntityList(&map,&start)){
+    if (!initEntityList(&map, &start))
+    {
         return 0;
     }
 
     int input = 0;
-    int quit = 0;
+    char move = ' ';
     clock_t t;
-    while (!quit)
+    int counter = 1;
+    while (1)
     {
         t = clock();
-        input = ' ';
 
         printMap(win, &map);
-        //wprintw(win, "Enter (q to quit): \n");
         input = getch();
+        if (input != ERR)
+        {
+            move = input;
+        }
         wrefresh(win);
         flushinp();
-        switch (input)
+
+        if (move == 'q')
         {
-            case 'q':
-            quit = 1;
             wprintw(win, "Exiting the program.\n");
             break;
-            case 'w':
-            case 'a':
-            case 's':
-            case 'd':
-            moveTank(&map, &(start.me), input);
-            break;
         }
-        
+
+        if (counter == framePerMove)
+        {
+            moveTank(&map, &(start.me), move);
+            counter = 0;
+        }
+
+        wclear(win);
+
+        counter++;
+
         t = clock() - t;
         usleep(frameMicroSec - 1000000 * t / CLOCKS_PER_SEC);
-        wclear(win);
     }
     closeMap(&map);
     closeEntities(&start);
@@ -65,10 +72,11 @@ int main()
     return 0;
 }
 
-WINDOW *initCursesWindow(){
-    WINDOW* w = newwin(0, 0, 0, 0);
+WINDOW *initCursesWindow()
+{
+    WINDOW *w = newwin(0, 0, 0, 0);
     refresh();
-    nodelay(stdscr,true);
+    nodelay(stdscr, true);
     cbreak();
     keypad(stdscr, true);
     noecho();
